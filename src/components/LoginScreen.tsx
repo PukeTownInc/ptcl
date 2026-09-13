583ff48ba2a398500d8e6b5d48831554393bc5bd
PukeTownInc <Puketowninc@gmail.com>
2026-09-13 15:25:00 +0100

    Change login screen logo height from 220px to 200px
    Adjusted logo height for better display.

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -32,7 +32,7 @@ export function LoginScreen({ onSkip }: Props) {
             src="/logo.png" 
             alt="Logo" 
             style={{ 
-              height: '220px', 
+              height: '200px', 
               width: 'auto',
               objectFit: 'contain',
               display: 'block'
