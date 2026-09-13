e2053f564950f983ef9d94807360f198c1d7ff63
PukeTownInc <Puketowninc@gmail.com>
2026-09-13 15:23:18 +0100

    Login screen logo size increase
    Increased logo height from 140px to 220px in LoginScreen.

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -32,7 +32,7 @@ export function LoginScreen({ onSkip }: Props) {
             src="/logo.png" 
             alt="Logo" 
             style={{ 
-              height: '140px', 
+              height: '220px', 
               width: 'auto',
               objectFit: 'contain',
               display: 'block'
